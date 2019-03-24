dd9e0a82638e24fca91fee2f21611a83dd8b8535
Nisarg Shah <npshah9856@gmail.com>
2019-03-23 17:33:02 -0700

    Added structure to the code

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../assets/css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+
+            Hello World!
+
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
