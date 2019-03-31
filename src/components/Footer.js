d99938e07125f75563cf04332c710d68e3c15957
Nisarg Shah <npshah9856@gmail.com>
2019-03-31 16:10:27 -0700

    Added 3 new components

---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "../assets/css/Footer.css";
+import { FaGithub, FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa";
+
+const Footer = props => {
+  return (
+    <div className="Footer-wrapper">
+      <footer className="Footer">
+        contact us
+        <div className="icons">
+          <a
+            href="https://github.com/"
+            className="icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size="2vw" />
+          </a>
+          <a
+            href="https://twitter.com/"
+            className="icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter size="2vw" />
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            className="icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook size="2vw" />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            className="icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram size="2vw" />
+          </a>
+        </div>
+        <p className="copyright">Giv &copy; {new Date().getFullYear()}</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/FormPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../assets/css/Form.css";
+
+const Form = props => {
+  return (
+    <div className="Form-wrapper">
+      Signup for product updates:
+      <br />
+      <input type="email" />
+    </div>
+  );
+};
+
+export default Form;
