a1100557140880e4b0acaa4acd087d0a77a599bf
Ryan Luu <ryanluu12345@gmail.com>
2019-04-02 02:30:49 -0700

    Added landing page and why page with overlay and proper stylings

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import "../assets/css/App.css";
 
+import LandingPage from "./LandingPage.js";
 import Footer from "./Footer.js";
 import PartnerPage from "./PartnerPage.js";
+import WhyPage from "./WhyPage";
 import Form from "./FormPage.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <LandingPage />
+        <WhyPage />
         <Form />
         <PartnerPage />
         <Footer />
---
+++ src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../assets/css/LandingPage.css";
+
+const LandingPage = props => {
+  return (
+    <div className="landing-page">
+      <div className="app-flex-title">
+        <h2 className="giv-title">giv</h2>
+        <h2 className="giv-slogan">charitable giving, reimagined</h2>
+      </div>
+      <div className="nav-button">Placeholder for nav button</div>
+    </div>
+  );
+};
+
+export default LandingPage;
