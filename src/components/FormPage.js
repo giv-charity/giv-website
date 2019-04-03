431476102fca4b622ea929bbe5b84da976bbd2ca
Ryan Luu <ryanluu12345@gmail.com>
2019-04-03 10:17:51 -0700

    Added styling for cards, signup portion, and overall responsiveness for cards and signup

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -4,9 +4,19 @@ import "../assets/css/Form.css";
 const Form = props => {
   return (
     <div className="form-wrapper">
-      Signup for product updates:
-      <br />
-      <input type="email" />
+      <h2 className="form-title">signup for updates</h2>
+      <div className="form-inputs">
+        <input
+          className="email-input"
+          type="email"
+          placeholder="enter your email address"
+        />
+        <input
+          className="signup-button"
+          type="button"
+          value="get early access"
+        />
+      </div>
     </div>
   );
 };
